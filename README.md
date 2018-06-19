0483ef83e78d51f836cab63c51dbdec514ef4fa7
Amanchouhan192 <amanchouhan192@gmail.com>
2018-06-19 13:44:15 +0530

    adding python file:smile:

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Eckovation_ml-
+this is the repository in which the python and ml programm is written ....keep Hacking!!!!!
---
+++ intersection.py
@@ -0,0 +1,8 @@
+v1 = [1,23,45,67,1,24]
+v2 = [2,45,5,1,222,99]
+ans =[]
+for i in v1:
+    for j in v2:
+        if i == j:
+            print i
+            ans.append
